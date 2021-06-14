08fdaee6c97cd37b34955ca1c201168b181060cd
Zhexian Dong <DongZh@ZhexianDong2023.attlocal.net>
2021-06-13 22:43:53 -0500

    convert integer to string

--- program1/myy1.py
+++ program1/myy1.py
@@ -9,3 +9,10 @@
 #print("/___|")
 
 #3
+character_name = "Tom"
+#character_age = "45"
+character_age2 = 45
+print("Once there was a man name " + character_name + ",")
+print("He is " + str(character_age2) + " years old.")
+print("Remembering that day " + character_name + "want to swim in the baby's pool,")
+print("The manager stop him cause he is " + str(character_age2 - 13) + " years older than the limit age.")
